e887982c29b820d9b29bc25a278697c162168a65
Anuj Paul <paulanuj27@gmail.com>
2024-06-20 11:59:57 +0600

    Create and input vector

--- Program.cpp
+++ Program.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main ()
 {
-    cout<<"Hello Anuj Boss"<<endl;
+    //create vector with n size 
+    int n;
+    cin>>n;
+    vector <int> v;
+    
+    //Input This vector with indexing for loops 
+    for (int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
 
     return 0;
 }
